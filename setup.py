c822fd35c8bfda423f8a4bdd77b2875a6b33b2b8
Ahmed T. Youssef <xmonader@gmail.com>
2018-01-16 13:32:28 +0200

    setup.py starting

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    # can't have the entry_points option here.
+    from distutils.core import setup
+
+setup(name='flask_itsyouonline',
+      version='1.0.0',
+      author="Ahmed T. Youssef",
+      author_email="xmonader@gmail.com",
+      description='Itsyou.online middleware for Flask.',
+      long_description='Itsyou.online middleware for Flask.',
+      py_modules=['flask_itsyouonline'],
+      url="http://github.com/xmonader/flask_itsyouonline",
+      license='BSD 3-Clause License',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      )
